90cecab985833739415906e0cb4b30259c8cddb0
Wunhyeon <42367317+Wunhyeon@users.noreply.github.com>
2024-07-07 22:51:57 +0900

    [Silver III] Title: 빈도 정렬, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/2910. 빈도 정렬/빈도 정렬.cc
+++ 백준/Silver/2910. 빈도 정렬/빈도 정렬.cc
@@ -1,56 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
 int n, c;
-map<int, int> mp;
 vector<int> v;
+map<int, int> cnt;
+map<int, int> order;
+int num;
 
-bool cmp(pair<int, int> a, pair<int, int> b)
+bool cmp(int a, int b)
 {
-    return a.second > b.second;
+    if (cnt[a] == cnt[b])
+    {
+        return order[a] < order[b];
+    }
+    return cnt[a] > cnt[b];
 }
 
 int main()
 {
-
-    // 숫자를 받는다.
-    // 맵에 빈도수를 저장한다.
-    // 벡터에 순서대로 저장한다.
-    // 그러니깐 앞에서부터 돌면서 맵에 없으면 1로 넣고, 있으면 ++을 해준다.
-    // 맵에 없으면 1로 넣으면서 벡터에도 추가, 맵에 없으면 벡터에 추가 X
-    // 2중 벡터를 만든다. 형식은 {{숫자, 빈도수}, {숫자, 빈도수}} 이런식.
-    // (1중)벡터를 반복문을 돌며 맵을 참조해 위에서 만든 2중벡터를 채운다.
-    // 2중 벡터를 정렬한다. 빈도수로
-    // sort(v.begin()[1], v.end()[1])이런식으로 하면 되지 않을까? => 안됌. 커스텀 정렬함수 cmp를 만들어줬다.
-    // 2중 벡터를 반복문을 돌며 출력한다.
-    // 출력할 때, 빈도수만큼 돌고, 다음으로 넘어간다.
-
     cin >> n >> c;
-    for (int i = 0; i < n; i++)
+    for (int i = 1; i <= n; i++)
     {
-        int j;
-        cin >> j;
-        if (!mp[j])
-            v.push_back(j);
-        mp[j]++;
+        cin >> num;
+        v.push_back(num);
+        cnt[num]++;
+        if (!order[num])
+        {
+            order[num] = i;
+        }
     }
-
-    vector<pair<int, int>> v2;
+    sort(v.begin(), v.end(), cmp);
 
     for (int i = 0; i < v.size(); i++)
     {
-        v2.push_back({v[i], mp[v[i]]});
+        cout << v[i] << " ";
     }
-
-    stable_sort(v2.begin(), v2.end(), cmp);
-
-    for (int i = 0; i < v2.size(); i++)
-    {
-        int j = v2[i].second;
-        for (int k = 0; k < j; k++)
-        {
-            cout << v2[i].first << " ";
-        }
-    }
-
     return 0;
 }
