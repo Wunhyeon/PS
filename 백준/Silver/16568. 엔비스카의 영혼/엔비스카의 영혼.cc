12a47981ac6ca55556b985e4be4354c1e1bab938
Wunhyeon <42367317+Wunhyeon@users.noreply.github.com>
2024-07-08 14:30:03 +0900

    [Silver I] Title: 엔비스카의 영혼, Time: 8 ms, Memory: 5928 KB -BaekjoonHub

--- 백준/Silver/16568. 엔비스카의 영혼/엔비스카의 영혼.cc
+++ 백준/Silver/16568. 엔비스카의 영혼/엔비스카의 영혼.cc
@@ -2,63 +2,27 @@
 using namespace std;
 int N, A, B;
 int mn = 10000001;
-int visited[1000004];
-struct str
-{
-    int a;
-    int b;
-};
-void go(int n, int a, int b, int cnt)
-{
-    if (n < 0)
-    {
-        return;
-    }
-    if (n == 0)
-    {
-        mn = min(mn, cnt);
-        return;
-    }
-
-    go(n - 1 - b, a, b, cnt + 1);
-    go(n - 1 - a, a, b, cnt + 1);
-    go(n - 1, a, b, cnt + 1);
-}
-
-void bfs(int a, int b)
-{
-    queue<int> q;
-    q.push(0);
-    visited[0] = 1;
-    while (q.size())
-    {
-        int cur = q.front();
-        int curD = visited[cur];
-        q.pop();
-        if (cur == N)
-        {
-            mn = min(mn, curD);
-            continue;
-        }
-        for (int next : {cur + 1, cur + a + 1, cur + b + 1})
-        {
-            if (next <= N && (!visited[next] || visited[next] > curD + 1))
-            {
-                q.push(next);
-                visited[next] = curD + 1;
-            }
-        }
-    }
-}
+int dp[1000004]; // 위치가 n일때의 최소 초
 
 int main()
 {
     cin >> N >> A >> B;
     // go(N, A, B, 0);
-    // dp[N] = 0;
-    bfs(A, B);
 
-    cout << mn - 1 << "\n";
+    for (int i = 1; i <= N; i++)
+    {
+        dp[i] = dp[i - 1] + 1;
+        if (i - A - 1 >= 0)
+        {
+            dp[i] = min(dp[i - A - 1] + 1, dp[i]);
+        }
+        if (i - B - 1 >= 0)
+        {
+            dp[i] = min(dp[i - B - 1] + 1, dp[i]);
+        }
+    }
+
+    cout << dp[N] << "\n";
 
     return 0;
 }
