1ba6caba574e2156cc553d5d5681195a5676c785
Wunhyeon <42367317+Wunhyeon@users.noreply.github.com>
2024-07-08 23:52:05 +0900

    [Silver V] Title: 비밀번호 발음하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/4659. 비밀번호 발음하기/비밀번호 발음하기.cc
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+string s;
+int main()
+{
+    while (1)
+    {
+        cin >> s;
+        if (s == "end")
+        {
+            break;
+        }
+        bool flag = true;
+        bool mo = false;
+        int jaCnt = 0;
+        int moCnt = 0;
+        char pre = '\0';
+        for (int i = 0; i < s.size(); i++)
+        {
+            if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u')
+            {
+                mo = true;
+                moCnt++;
+                jaCnt = 0;
+            }
+            else
+            {
+                jaCnt++;
+                moCnt = 0;
+            }
+
+            if (moCnt >= 3 || jaCnt >= 3)
+            {
+                flag = false;
+                break;
+            }
+
+            // if (pre == s[i])
+            // {
+            //     if (pre != 'e' && pre != 'o')
+            //     {
+            //         flag = false;
+            //         break;
+            //     }
+            // }
+            if (pre && s[i] != 'e' && s[i] != 'o')
+            {
+                if (pre == s[i])
+                {
+                    // cout << "ai" << '\n';
+                    flag = false;
+                    break;
+                }
+            }
+
+            pre = s[i];
+        }
+
+        if (mo && flag)
+        {
+            cout << "<" << s << "> is acceptable." << "\n";
+        }
+        else
+        {
+            cout << "<" << s << "> is not acceptable." << "\n";
+        }
+    }
+    return 0;
+}
