5d2b3b1824ad1af0e6e1e69364edb3594fa14d4f
임재현 <42367317+Wunhyeon@users.noreply.github.com>
2024-07-26 05:47:18 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int n;
+string s;
+
+int main()
+{
+    cin >> s;
+    cin >> n;
+    cout << s[n - 1] << "\n";
+    return 0;
+}
