2798a0346c1a6c25a4c7f5b1ab935b8fb8a5aa69
임재현 <42367317+Wunhyeon@users.noreply.github.com>
2024-08-11 00:00:42 +0900

    [Bronze V] Title: Multiply, Time: 0 ms, Memory: 2020 KB, Score: 40 point -BaekjoonHub

---
+++ 백준/Bronze/22193. Multiply/Multiply.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+// int t;
+int q1, q2;
+long long a, b;
+int main()
+{
+    cin >> q1 >> q2;
+    cin >> a >> b;
+    cout << a * b << "\n";
+
+    return 0;
+}
