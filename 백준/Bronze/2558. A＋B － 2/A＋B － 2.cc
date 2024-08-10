fe41f1e62837f67b92a5f06d8b6a2be4b5ce65a8
임재현 <42367317+Wunhyeon@users.noreply.github.com>
2024-08-11 00:04:27 +0900

    [Bronze V] Title: A+B - 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+// int t;
+
+long long a, b;
+int main()
+{
+
+    cin >> a >> b;
+    cout << a + b << "\n";
+
+    return 0;
+}
