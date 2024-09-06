76c5201c5bbe7e6fd7ccd2a52178ce8528a62b37
임재현 <42367317+Wunhyeon@users.noreply.github.com>
2024-09-07 00:53:29 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int n1, n2, n3, n4, n5, n6;
+
+int main()
+{
+    // 1 1 2 2 2 8
+    cin >> n1 >> n2 >> n3 >> n4 >> n5 >> n6;
+    cout << 1 - n1 << " " << 1 - n2 << " " << 2 - n3 << " " << 2 - n4 << " " << 2 - n5 << " " << 8 - n6 << "\n";
+
+    return 0;
+}
